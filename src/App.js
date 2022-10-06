eb8a205b4b1ba717079d791c795bbad0cd2eec69
Adarsha A <adarshaadi1997@gmail.com>
2022-10-06 19:52:00 +0530

    bout and Home PAge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Navbar from './component/Navbar';
+import About from './component/about';
 import {BrowserRouter as Router,Route,Link,Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <Router>
-
       <Navbar/>
+      <div className='container'>
+     <Route exact path = "/about" component = {About} />
+
+      </div>
+
+
+      
+
     </Router>
   );
 }
---
+++ src/component/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About =() =>{
+    return (
+        <>
+        <h1>Working on React Project</h1>
+        <p>Hii Cool Buddy</p>
+        </>
+    )
+}
+
+export default About;
